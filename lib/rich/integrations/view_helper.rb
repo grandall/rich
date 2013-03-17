ec32edbca053b42fd979afb5d0748b814fca1baa
Graham Randall <grandall@normalmodes.com>
2013-03-16 20:59:35 -0700

    Brought view_helper up to functional equivalency with form_helper.

--- lib/rich/integrations/view_helper.rb
+++ lib/rich/integrations/view_helper.rb
@@ -15,17 +15,37 @@ module Rich
         output_buffer
       end
 
-      def rich_picker_tag(name, content = nil, options = {})
+      def rich_picker_tag(name, image = nil, options = {})
         element_id = sanitize_to_id(name)
         options = { :language => I18n.locale.to_s }.merge(options)
-        input_html = { :id => element_id }.merge( options.delete(:input_html) || {} )
+        input_html = { 'id' => element_id }.merge( options.delete(:input_html) || {} )
+
+        editor_options = Rich.options(options[:config])
+
+        content = nil
+        unless image == nil
+          image_url = image.url(:content)
+          content = image.id if options[:hidden_input]
+        else
+          image_url = editor_options[:placeholder_image]
+        end
 
         output_buffer = ActiveSupport::SafeBuffer.new
-        output_buffer << text_field_tag(name, content, input_html)
+        if editor_options[:hidden_input] == true
+          output_buffer << hidden_field_tag(name, content, input_html)
+        else
+          output_buffer << text_field_tag(name, content, input_html)
+        end
 
-        output_buffer << link_to(I18n.t('picker_browse'), Rich.editor[:richBrowserUrl], :class => 'button')
-        output_buffer << image_tag(@object.send(method), :class => 'rich-image-preview', :style => 'height: 100px')
-        output_buffer << javascript_tag("$(function(){$('##{input_html['id']} a').click(function(e){ e.preventDefault(); assetPicker.showFinder('##{input_html['id']}', #{options.to_json.html_safe})})})")
+        output_buffer << link_to(I18n.t('picker_browse'), Rich.editor[:richBrowserUrl], :class => 'btn')
+        if image_url.present?
+          output_buffer << content_tag("ul", :class => 'thumbnails') do
+            content_tag("li", :class => 'span12') do
+              image_tag(image_url, :class => 'rich-image-preview', :size => '260x180')
+            end
+          end
+        end
+        output_buffer << javascript_tag("$(document).ready(function(){$('##{input_html['id']} + a').click(function(e){ e.preventDefault(); assetPicker.showFinder('##{input_html['id']}', #{editor_options.to_json.html_safe})})})")
         output_buffer
       end
     end
