54141477d3beef4aae7ce7bdaeeb1fb3f2be5e32
Graham Randall <grandall@normalmodes.com>
2014-03-02 20:42:12 -0600

    Display only videos for video type in rich_picker.

--- app/controllers/rich/files_controller.rb
+++ app/controllers/rich/files_controller.rb
@@ -7,20 +7,15 @@ module Rich
     
     def index
       @type = params[:type]
-      
-      if(params[:scoped] == 'true')
-        if(@type == "image")
-          @items = RichFile.images.order("created_at DESC").where("owner_type = ? AND owner_id = ?", params[:scope_type], params[:scope_id]).page params[:page]
-        else
-          @items = RichFile.files.order("created_at DESC").where("owner_type = ? AND owner_id = ?", params[:scope_type], params[:scope_id]).page params[:page]
-        end        
+      query = if @type == 'image'
+        RichFile.images
+      elsif @type == 'video'
+        RichFile.videos
       else
-        if(@type == "image")
-          @items = RichFile.images.order("created_at DESC").page params[:page]
-        else
-          @items = RichFile.files.order("created_at DESC").page params[:page]
-        end        
-      end
+        RichFile.files
+      end.order('created_at DESC')
+      query = query.where(:owner_type => params[:scope_type], :owner_id => :params[:scope_id]) if params[:scoped] == 'true'
+      @items = query.page params[:page]
       
       # stub for new file
       @rich_asset = RichFile.new
